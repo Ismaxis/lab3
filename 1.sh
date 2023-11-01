dc9b4432696ac71767bf53ffe57c7171137cc59a
Ismaxis <Ismaxis@github.com>
2023-11-01 23:00:50 +0300

    + 2.sh

--- 1.sh
+++ 1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-DIR="."
+DIR="./out"
 URL="www.net_nikogo.ru"
 
 DATE_TIME=$(date +"%m-%d-%Y %H:%M")
 
-mkdir $DIR/test && echo "catalog test was created successfully" > $DIR/report && touch $DIR/test/"$DATE_TIME"
+mkdir $DIR/test 2> /dev/null && echo "catalog test was created successfully" > $DIR/report && touch $DIR/test/"$DATE_TIME"
 
-ping $URL || echo "$DATE_TIME: can't ping $URL" >> $DIR/report
+ping $URL 2> /dev/null || echo "$DATE_TIME: can't ping $URL" >> $DIR/report
---
+++ 2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# sudo atd
+echo "bash 1.sh" | at now + 2 minute
+
+# tail -f out/report
